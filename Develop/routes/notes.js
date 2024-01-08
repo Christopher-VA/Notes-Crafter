980cffc41e1faad466311f7e4819806bd47825d0
Christopher-VA <cvonaltenstadt@gmail.com>
2024-01-08 17:09:01 +0800

    completing notes route attempt

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,3 +1,31 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const notes = require('express').Router();
+// const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNotes = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNotes, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNotes,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting notes');
+    }
+});
+
+module.exports = notes;
